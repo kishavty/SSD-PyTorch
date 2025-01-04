86104015300a1d586ac37b4f76fb0836ac026a95
Katarzyna Kudełko <107676210+kishavty@users.noreply.github.com>
2025-01-04 23:39:16 +0100

    fix: fixed error with float numbers

--- tools/infer.py
+++ tools/infer.py
@@ -37,117 +37,62 @@ def get_iou(det, gt):
 
 
 def compute_map(det_boxes, gt_boxes, iou_threshold=0.5, method='area', difficult=None):
-    # det_boxes = [
-    #   {
-    #       'person' : [[x1, y1, x2, y2, score], ...],
-    #       'car' : [[x1, y1, x2, y2, score], ...]
-    #   }
-    #   {det_boxes_img_2},
-    #   ...
-    #   {det_boxes_img_N},
-    # ]
-    #
-    # gt_boxes = [
-    #   {
-    #       'person' : [[x1, y1, x2, y2], ...],
-    #       'car' : [[x1, y1, x2, y2], ...]
-    #   },
-    #   {gt_boxes_img_2},
-    #   ...
-    #   {gt_boxes_img_N},
-    # ]
-
     gt_labels = {cls_key for im_gt in gt_boxes for cls_key in im_gt.keys()}
     gt_labels = sorted(gt_labels)
 
     all_aps = {}
-    # average precisions for ALL classes
     aps = []
     for idx, label in enumerate(gt_labels):
-        # Get detection predictions of this class
         cls_dets = [
             [im_idx, im_dets_label] for im_idx, im_dets in enumerate(det_boxes)
             if label in im_dets for im_dets_label in im_dets[label]
         ]
-
-        # cls_dets = [
-        #   (0, [x1_0, y1_0, x2_0, y2_0, score_0]),
-        #   ...
-        #   (0, [x1_M, y1_M, x2_M, y2_M, score_M]),
-        #   (1, [x1_0, y1_0, x2_0, y2_0, score_0]),
-        #   ...
-        #   (1, [x1_N, y1_N, x2_N, y2_N, score_N]),
-        #   ...
-        # ]
-
-        # Sort them by confidence score
         cls_dets = sorted(cls_dets, key=lambda k: -k[1][-1])
-
-        # For tracking which gt boxes of this class have already been matched
         gt_matched = [[False for _ in im_gts[label]] for im_gts in gt_boxes]
-        # Number of gt boxes for this class for recall calculation
         num_gts = sum([len(im_gts[label]) for im_gts in gt_boxes])
         num_difficults = sum([sum(difficults_label[label]) for difficults_label in difficult])
 
         tp = [0] * len(cls_dets)
         fp = [0] * len(cls_dets)
 
-        # For each prediction
         for det_idx, (im_idx, det_pred) in enumerate(cls_dets):
-            # Get gt boxes for this image and this label
             im_gts = gt_boxes[im_idx][label]
             im_gt_difficults = difficult[im_idx][label]
-
             max_iou_found = -1
             max_iou_gt_idx = -1
 
-            # Get best matching gt box
             for gt_box_idx, gt_box in enumerate(im_gts):
                 gt_box_iou = get_iou(det_pred[:-1], gt_box)
                 if gt_box_iou > max_iou_found:
                     max_iou_found = gt_box_iou
                     max_iou_gt_idx = gt_box_idx
-            # TP only if iou >= threshold and this gt has not yet been matched
             if max_iou_found >= iou_threshold:
                 if not im_gt_difficults[max_iou_gt_idx]:
                     if not gt_matched[im_idx][max_iou_gt_idx]:
-                        # If tp then we set this gt box as matched
                         gt_matched[im_idx][max_iou_gt_idx] = True
                         tp[det_idx] = 1
                     else:
                         fp[det_idx] = 1
             else:
                 fp[det_idx] = 1
-
-        # Cumulative tp and fp
         tp = np.cumsum(tp)
         fp = np.cumsum(fp)
 
         eps = np.finfo(np.float32).eps
-        # recalls = tp / np.maximum(num_gts, eps)
         recalls = tp / np.maximum(num_gts - num_difficults, eps)
         precisions = tp / np.maximum((tp + fp), eps)
 
         if method == 'area':
             recalls = np.concatenate(([0.0], recalls, [1.0]))
             precisions = np.concatenate(([0.0], precisions, [0.0]))
-
-            # Replace precision values with recall r with maximum precision value
-            # of any recall value >= r
-            # This computes the precision envelope
             for i in range(precisions.size - 1, 0, -1):
                 precisions[i - 1] = np.maximum(precisions[i - 1], precisions[i])
-            # For computing area, get points where recall changes value
             i = np.where(recalls[1:] != recalls[:-1])[0]
-            # Add the rectangular areas to get ap
             ap = np.sum((recalls[i + 1] - recalls[i]) * precisions[i + 1])
         elif method == 'interp':
             ap = 0.0
             for interp_pt in np.arange(0, 1 + 1E-3, 0.1):
-                # Get precision values for recall values >= interp_pt
                 prec_interp_pt = precisions[recalls >= interp_pt]
-
-                # Get max of those precision values
                 prec_interp_pt= prec_interp_pt.max() if prec_interp_pt.size>0.0 else 0.0
                 ap += prec_interp_pt
             ap = ap / 11.0
@@ -158,21 +103,17 @@ def compute_map(det_boxes, gt_boxes, iou_threshold=0.5, method='area', difficult
             all_aps[label] = ap
         else:
             all_aps[label] = np.nan
-    # compute mAP at provided iou threshold
     mean_ap = sum(aps) / len(aps)
     return mean_ap, all_aps
 
 
 def load_model_and_dataset(args):
-    # Read the config file #
     with open(args.config_path, 'r') as file:
         try:
             config = yaml.safe_load(file)
         except yaml.YAMLError as exc:
             print(exc)
     print(config)
-    ########################
-
     dataset_config = config['dataset_params']
     model_config = config['model_params']
     train_config = config['train_params']
@@ -206,14 +147,14 @@ def infer(args):
 
     num_samples = 5
     for i in tqdm(range(num_samples)):
-        dataset_idx = random.randint(0, len(voc))
+        dataset_idx = random.randint(0, len(voc) - 1)
         im_tensor, target, fname = voc[dataset_idx]
+        im_tensor = im_tensor.float()
         _, ssd_detections = model(im_tensor.unsqueeze(0).to(device), [target])
 
         gt_im = cv2.imread(fname)
         h, w = gt_im.shape[:2]
         gt_im_copy = gt_im.copy()
-        # Saving images with ground truth boxes
         for idx, box in enumerate(target['bboxes']):
             x1, y1, x2, y2 = box.detach().cpu().numpy()
             x1, y1, x2, y2 = int(w*x1), int(h*y1), int(w*x2), int(h*y2)
@@ -237,15 +178,12 @@ def infer(args):
                         fontFace=cv2.FONT_HERSHEY_PLAIN)
         cv2.addWeighted(gt_im_copy, 0.7, gt_im, 0.3, 0, gt_im)
         cv2.imwrite('samples/output_ssd_gt_{}.png'.format(i), gt_im)
-
-        # Getting predictions from trained model
         boxes = ssd_detections[0]['boxes']
         labels = ssd_detections[0]['labels']
         scores = ssd_detections[0]['scores']
         im = cv2.imread(fname)
         im_copy = im.copy()
 
-        # Saving images with predicted boxes
         for idx, box in enumerate(boxes):
             x1, y1, x2, y2 = box.detach().cpu().numpy()
             x1, y1, x2, y2 = int(w * x1), int(h * y1), int(w * x2), int(h * y2)
